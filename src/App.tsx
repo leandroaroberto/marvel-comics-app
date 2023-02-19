0b6bbd604f7dff4c1717984e4a04716c0a35ec34
Leandro Aparecido Roberto <leandro.roberto@ciandt.com>
2023-02-18 21:57:04 -0300

    Modal

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import { Md5 } from 'ts-md5'
 import Card from './components/Card'
 import Header from './components/Header'
 import Search from './components/Search'
+import Modal from './components/Modal'
 
 const TIME_STAMP = Math.floor(Date.now() / 1000)
 const HASH = Md5.hashStr(TIME_STAMP + import.meta.env.VITE_API_PRIVATE_KEY + import.meta.env.VITE_API_PUBLIC_KEY)
 
+
 function App() {
 
+  const [showModal, setShowModal] = useState(false);
+  const [heroId, setHeroId] = useState(0);
   const [comics, setComics] = useState([])
 
   useEffect(() => {
@@ -22,8 +26,13 @@ function App() {
     });
   }, [])
 
-  return (
-    <>
+  const showHeroDetails = (id: number ) => {
+    setShowModal(true)
+    setHeroId(id)
+  }
+
+    return (
+      <>
       <Header>
         Marvel Comics App
       </Header>
@@ -31,18 +40,20 @@ function App() {
       <div className="flex flex-wrap justify-evenly items-center max-w-full py-2 m-5">
       {
         comics.length ? comics.map((comic : any)  =>
-          <Card
-            key={comic.id}
-            title={comic.title}
-            image={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
-          />
-          )
-          :
-          <div className="flex justify-center items-center max-w-full">
+        <Card
+        key={comic.id}
+        title={comic.title}
+        image={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+        onClick={() => showHeroDetails(comic.id)}
+        />
+        )
+        :
+        <div className="flex justify-center items-center max-w-full">
             <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921"/>
           </div>
       }
       </div>
+      <Modal showModal={showModal} setShowModal={setShowModal} heroId={heroId}/>
     </>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,21 @@
 interface CardProps {
     title: string;
     image: string;
+    onClick : any;
 }
 
-function Card({ title, image } : CardProps) {
+function Card({ title, image, onClick } : CardProps) {
   return (
     <div className="border w-80 shadow-lg hover:shadow-blue-600/50 p-6 my-2">
       <h2 className="text-center font-bold py-2">{title}</h2>
       <img src={image} className="w-72 h-auto"/>
       <p className="text-center font-light py-4">
-        <button className="border-2 border-sky-100/100 rounded-md text-center bg-sky-500  hover:bg-white hover:text-black text-cyan-50 p-3">More info</button>
+        <button
+          className="border-2 border-sky-100/100 rounded-md text-center bg-sky-500  hover:bg-white hover:text-black text-cyan-50 p-3"
+          onClick={onClick}
+        >
+          More info
+        </button>
       </p>
     </div>
   )
