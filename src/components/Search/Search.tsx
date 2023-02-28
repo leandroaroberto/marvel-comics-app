6ac157618799aceca16cc2eb328330fe44f68a5e
Leandro Aparecido Roberto <leandro.roberto@ciandt.com>
2023-02-28 08:56:16 -0300

    Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Search = () => {
+  const [ hero, setHero ] = useState('')
+
+  const searchByHero = (e : any) => {
+    setHero(e.target.value);
+  }
+
   return (
     <div className="flex justify-center items-center py-4 mx-5">
-        <input className="border rounded-md p-2 w-full" placeholder="Search for your favorite Marvel hero"/>
+        <input
+          className="border rounded-md p-2 w-full"
+          placeholder="Search for your favorite Marvel hero"
+          onChange={searchByHero}
+          value={hero}
+        />
     </div>
   )
 }
