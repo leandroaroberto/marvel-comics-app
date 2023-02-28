335fee383557ee715c3e91543943501e0c3b6b38
Leandro Aparecido Roberto <leandro.roberto@ciandt.com>
2023-02-27 21:31:59 -0300

    First test for Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex justify-center font-sans text-sm py-4" test-id="footerLabel">Data provided by Marvel. © 2014 Marvel</div>
+    <div className="flex justify-center font-sans text-sm py-4">
+      <span>
+        Data provided by
+      </span>
+      &nbsp;
+       <span data-testid="footerLabelCompany">
+       Marvel.
+       </span>
+       &nbsp;
+       <span>
+        ©
+       </span>
+       &nbsp;
+       <span data-testid="footerLabelYear">
+       2014
+       </span>
+       &nbsp;
+       <span>
+        Marvel
+       </span>
+    </div>
   )
 }
 
