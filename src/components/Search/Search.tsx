1211e6c8131204f1c2a1d0e3caecbec2de95fc42
Leandro Aparecido Roberto <leandro.roberto@ciandt.com>
2023-03-10 00:10:36 -0300

    Updating Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -5,24 +5,33 @@ import { api } from '../../api';
 const TIME_STAMP = Math.floor(Date.now() / 1000)
 const HASH = Md5.hashStr(TIME_STAMP + import.meta.env.VITE_API_PRIVATE_KEY + import.meta.env.VITE_API_PUBLIC_KEY)
 
-const Search = ({setCharacterId} : any) => {
+interface CharacterResultsProps {
+  id : number;
+}
+
+const Search = ({setCharacterId} :any) => {
   const [ hero, setHero ] = useState('')
+  const [debouncedHero, setDebouncedHero] = useState(hero);
 
   const searchByHero = (e : any) => {
-    setHero(e.target.value);
+    setDebouncedHero(e.target.value)
   }
+
+  useEffect(() => {
+    const timer = setTimeout(() => setHero(debouncedHero), 1000);
+    return () => clearTimeout(timer);
+}, [debouncedHero])
 
   useEffect(() => {
     api.get(`/characters?nameStartsWith=${hero}r&limit=10&ts=${TIME_STAMP}&apikey=${import.meta.env.VITE_API_PUBLIC_KEY}&hash=${HASH}`)
     .then(function (response: any) {
-      //console.log(response.data.data.results);
-      const characterResults = response.data.data.results.map((result : any, index : number) => result.id);
-      console.log('character:', characterResults[characterResults.length - 1])
-      if (response.data.data.results.length) {
-        setCharacterId(characterResults[characterResults.length - 1])
-      } else {
-        setCharacterId(0)
-      }
+      const data = response.data.data.results
+      const characterResults = data.reduce(
+        (accumulator: number, currentValue: CharacterResultsProps) => currentValue.id,
+        0,
+      );
+      setCharacterId(characterResults)
+      console.log('character:', characterResults)
     })
     .catch(function (error) {
       console.log(error);
@@ -35,7 +44,7 @@ const Search = ({setCharacterId} : any) => {
           className="border rounded-md p-2 w-full"
           placeholder="Search for your favorite Marvel hero"
           onChange={searchByHero}
-          value={hero}
+          value={debouncedHero}
         />
     </div>
   )
